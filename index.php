688cedc724dcfbc068a8686018bbffd8f674acb8
TJKnigge <tjknigge1960@kpnmail.nl>
2017-11-09 14:58:22 +0100

    Verandert van buttons naar table nu met plaatjes

--- index.php
+++ index.php
@@ -3,31 +3,45 @@
     <head>
         <meta charset="UTF-8">
         <title>Memory</title>
+    
+        <script>
+        function schieten(a){
+            document.getElementById("knop"+a).src = 'panda.jpg';
+            alert(a);
+        }
+        </script>
+        
+    <style>
+    
+    .table {
+    background-color: #4CAF50; 
+    border: none;
+    color: red;
+    padding: 5px 5px;
+    width: 100px;
+    height: 100px;
+    
+}
+    
+    </style>
+    
     </head>
     
     
-    <script>
-    
-    
-    
-    </script>
+
     
     
     <body>
+        <table border="1">
         <?php
         
-        for($x = 1; $x < 11; $x++) {
-            
-            echo"<br>";
-            for($y = 1; $y < 11; $y++) {
-           
+        for($x = 1; $x < 6; $x++) {
+            echo"<tr>";
+            for($y = 1; $y < 5; $y++) {
                 $id= $x.$y;
-        
-       
-        
-            echo "<input onclick=schieten($id) type=button id=knop$id>";
-        
+            echo "<td><img id=$id src=panda.jpg onclick=knop($id)</td>";
             }
+            echo "</tr>";
         }
         
         
